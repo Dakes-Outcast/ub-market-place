4db995029e0f17dbdb8350d06762c1d412ce42ef
Perception <cloudoutcast1@gmail.com>
2024-11-08 23:46:40 +0200

    no 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { CartViewComponent } from './cart/cart-view/cart-view.component';
-import { LogInComponent } from './components/log-in/log-in.component';
+
 const routes: Routes = [
+  {path: 'log-in', component: LogInComponent},
   {path:'',redirectTo:'/products', pathMatch: 'full'},
   {path: 'products', component: ProductListComponent},
-  {path: 'cart', component: CartViewComponent},
- {path: 'log-in', component: LogInComponent}
-];
+  {path: 'cart', component: CartViewComponent}
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
